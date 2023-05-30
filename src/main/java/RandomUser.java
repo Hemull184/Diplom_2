85bd8fa3a64fe005f994cb9aea7a6ab408e2230c
Konstantin Adianov <kastan184@mail.ru>
2023-05-30 15:54:59 +0300

    add StepsUser

--- src/main/java/RandomUser.java
+++ src/main/java/RandomUser.java
@@ -3,10 +3,10 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class RandomUser {
 
     public static User generate() {
-        return new User("test@yandex.ru", "123456789", "Pavlik");
+        return new User("Pavlik48@yandex.ru", "123456789", "Pavlik48");
     }
 
     public static User random() {
-        return new User(RandomStringUtils.randomAlphabetic(10) + "@yandex.ru", "123456789", "Pavlik");
+        return new User(RandomStringUtils.randomAlphabetic(10) + "@yandex.ru", "123456789", "Pavlik48");
     }
 }
